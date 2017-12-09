403b6f2d0d246bae8b4f4ec63272a3186577a241
Adam Soffer <adam.soffer@digitalsurgeons.com>
2017-12-09 15:25:23 -0500

    Update test

--- test/splitter.js
+++ test/splitter.js
@@ -2,7 +2,7 @@ const web3 = global.web3;
 const Splitter = artifacts.require('./Splitter.sol')
 
 contract('Splitter', function(accounts) {
-  let deposit = web3.toWei(4, 'ether')
+  let deposit = web3.toWei(2, 'ether')
 
   const bobAddress = '0x8B578b413186cD75590372ACacB6FaC64e9EAD12'
   const carolAddress = '0xcd8148C45ABFF4b3F01faE5aD31bC96AD6425054'
@@ -13,16 +13,38 @@ contract('Splitter', function(accounts) {
     splitter = await Splitter.new({ from: accounts[0] })
   })
 
-  it('should split deposited amount equally between two accounts', async function() {
-    await splitter.deposit(bobAddress, carolAddress, {
-      from: accounts[0],
-      value: deposit
+  describe('deposit()', async function() {
+    it('should split deposited amount equally between bob and carol', async function() {
+      let bobBalanceBefore = await splitter.balances.call(bobAddress)
+      let carolBalanceBefore = await splitter.balances.call(carolAddress)
+      
+      await splitter.deposit(bobAddress, carolAddress, {
+        from: accounts[0],
+        value: deposit
+      })
+
+      let bobBalanceAfter = await splitter.balances.call(bobAddress)
+      let carolBalanceAfter = await splitter.balances.call(carolAddress)
+
+      assert.strictEqual(
+        parseInt(bobBalanceBefore.toString()),
+        0
+      )
+
+      assert.strictEqual(
+        parseInt(carolBalanceBefore.toString()),
+        0
+      )
+
+      assert.strictEqual(
+        parseInt(bobBalanceAfter.toString()),
+        deposit / 2
+      )
+
+      assert.strictEqual(
+        parseInt(carolBalanceAfter.toString()),
+        deposit / 2
+      )
     })
-    let bobBalance = await splitter.balances.call(bobAddress)
-    let carolBalance = await splitter.balances.call(carolAddress)
-    assert.strictEqual(
-      web3.fromWei(bobBalance.toString(), 'ether'),
-      web3.fromWei(carolBalance.toString(), 'ether')
-    )
-  })
+  });
 })
