8a505a6ccb8ce94ec2411a5feeda016f7dd1486b
Adam Soffer <adam.soffer@digitalsurgeons.com>
2017-12-09 12:57:43 -0500

    Make owner variable private and add getter instead

--- contracts/Mortal.sol
+++ contracts/Mortal.sol
@@ -2,14 +2,23 @@ pragma solidity ^0.4.4;
 
 
 contract Mortal {
-  address public owner;
+  address private owner;
 
   function Mortal() public {
     owner = msg.sender;
   }
 
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function getOwner() public view returns (address) {
+    return owner;
+  }
+
   /* Function to recover the funds on the contract */
-  function kill() public {
+  function kill() public onlyOwner {
     if (msg.sender == owner) {
       selfdestruct(owner);
     }
