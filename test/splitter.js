a68edc9686676a3eea837cd7167e2af643e01851
Adam Soffer <ads1018@gmail.com>
2018-01-23 19:27:47 -0500

    Use bigNumber's div function

--- test/splitter.js
+++ test/splitter.js
@@ -2,46 +2,56 @@ const web3 = require('../lib/web3')
 const Splitter = artifacts.require('./Splitter.sol')
 
 contract('Splitter', function(accounts) {
-  let deposit = web3.utils.toWei('2', 'ether')
-
   let splitter
+  let deposit = web3.utils.toWei('2', 'ether')
+  let carolContractBalanceBefore
+  let carolContractBalanceAfter
+  let bobContractBalanceBefore
+  let bobContractBalanceAfter
 
   before('should deploy Splitter', async function() {
     splitter = await Splitter.new({ from: accounts[0] })
+    bobContractBalanceBefore = await splitter.balances.call(accounts[0])
+    carolContractBalanceBefore = await splitter.balances.call(accounts[1])
   })
 
   describe('deposit()', async function() {
     it('should split deposit equally among bob and carol', async function() {
-      let bobBalanceBefore = await splitter.balances.call(accounts[0])
-      let carolBalanceBefore = await splitter.balances.call(accounts[1])
-
       await splitter.deposit(accounts[0], accounts[1], {
         from: accounts[3],
         value: deposit
       })
 
-      let bobBalanceAfter = await splitter.balances.call(accounts[0])
-      let carolBalanceAfter = await splitter.balances.call(accounts[1])
+      bobContractBalanceAfter = await splitter.balances.call(accounts[0])
+      carolContractBalanceAfter = await splitter.balances.call(accounts[1])
+
       assert.strictEqual(
-        bobBalanceAfter.minus(bobBalanceBefore).toString(10),
-        (deposit / 2).toString(10)
+        bobContractBalanceAfter.minus(bobContractBalanceBefore).toString(10),
+        web3.utils
+          .toBN(deposit)
+          .div(web3.utils.toBN(2))
+          .toString(10)
       )
+
       assert.strictEqual(
-        carolBalanceAfter.minus(carolBalanceBefore).toString(10),
-        (deposit / 2).toString(10)
+        carolContractBalanceAfter.sub(carolContractBalanceBefore).toString(10),
+        web3.utils
+          .toBN(deposit)
+          .div(web3.utils.toBN(2))
+          .toString(10)
       )
     })
   })
 
   describe('withdraw()', async function() {
-    it('account value should be 0 after withdraw', async function() {
+    it('bobs contract balance should be 0 after withdrawal', async function() {
       await splitter.withdraw({
         from: accounts[0]
       })
 
-      let bobBalanceAfter = await splitter.balances.call(accounts[0])
+      bobContractBalanceAfter = await splitter.balances.call(accounts[0])
 
-      assert.strictEqual(bobBalanceAfter.toString(10), '0')
+      assert.strictEqual(bobContractBalanceAfter.toString(10), '0')
     })
   })
 })
